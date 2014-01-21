df31449939e7e02b44fbf9a74d7779ccd038443d
John Oliva <joliva63@gmail.com>
2014-01-21 16:32:55 -0500

    Added blog and user actions.

--- actions/blog.js
+++ actions/blog.js
@@ -1,19 +1,171 @@
-exports.action = {
-  name:                   'blog',
-  description:            'blog',
-  blockedConnectionTypes: [],
-  outputExample:          {},
-  matchExtensionMimeType: false,
-  version:                1.0,
-  toDocument:             true,
-
-  inputs: {
-    required: [],
-    optional: [],
-  },
-
-  run: function(api, connection, next){
-    // your logic here
-    next(connection, true);
-  }
+exports.postAdd = {
+	name: "postAdd",
+	description: "I add a post",
+	inputs: {
+		required: ["userName", "password", "title", "content"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: true,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.blog.postAdd(connection.params.userName, connection.params.title, connection.params.content, function(error){
+			connection.error = error;
+			next(connection, true);
+		});
+	}
 };
+
+exports.postView = {
+	name: "postView",
+	description: "I view a post",
+	inputs: {
+		required: ["userName", "title"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: false,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.blog.postView(connection.params.userName, connection.params.title, function(error, post){
+			connection.error = error;
+			connection.response.post = post;
+			next(connection, true);
+		});
+	}
+};
+
+exports.postsList = {
+	name: "postsList",
+	description: "I list all of a user's posts",
+	inputs: {
+		required: ["userName"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: false,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.blog.postsList(connection.params.userName, function(error, posts){
+			connection.error = error;
+			connection.response.posts = posts;
+			next(connection, true);
+		});
+	}
+};
+
+exports.postEdit = {
+	name: "postEdit",
+	description: "I edit a post",
+	inputs: {
+		required: ["userName", "password", "title", "content"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: true,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.blog.postEdit(connection.params.userName, connection.params.title, connection.params.content, function(error){
+			connection.error = error;
+			next(connection, true);
+		});
+	}
+};
+
+exports.postDelete = {
+	name: "postDelete",
+	description: "I delete a post",
+	inputs: {
+		required: ["userName", "password", "title"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: true,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.blog.postDelete(connection.params.userName, connection.params.title, function(error){
+			connection.error = error;
+			next(connection, true);
+		});
+	}
+};
+
+exports.commentAdd = {
+	name: "commentAdd",
+	description: "I add a comment",
+	inputs: {
+		required: ["userName", "title", "commenterName", "comment"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: false,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.blog.commentAdd(connection.params.userName, connection.params.title, connection.params.commenterName, connection.params.comment, function(error){
+			connection.error = error;
+			next(connection, true);
+		});
+	}
+};
+
+exports.commentsView = {
+	name: "commentsView",
+	description: "I show all comments for a post",
+	inputs: {
+		required: ["userName", "title"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: false,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.blog.commentsView(connection.params.userName, connection.params.title, function(error, comments){
+			connection.error = error;
+			connection.response.comments = comments;
+			next(connection, true);
+		});
+	}
+};
+
+exports.commentDelete = {
+	name: "commentDelete",
+	description: "I add a comment",
+	inputs: {
+		required: ["userName", "password", "commentId", "title"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: true,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.blog.commentDelete(connection.params.userName, connection.params.title, connection.params.commentId, function(error){
+			connection.error = error;
+			next(connection, true);
+		});
+	}
+};
+
--- actions/users.js
+++ actions/users.js
@@ -1,19 +1,88 @@
-exports.action = {
-  name:                   'users',
-  description:            'users',
-  blockedConnectionTypes: [],
-  outputExample:          {},
-  matchExtensionMimeType: false,
-  version:                1.0,
-  toDocument:             true,
+exports.userAdd = {
+	name: "userAdd",
+	description: "I add a user",
+	inputs: {
+		required: ["userName", "password"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: false,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.users.add(connection.params.userName, connection.params.password, function(error){
+			connection.error = error;
+			next(connection, true);
+		});
+	}
+};
 
-  inputs: {
-    required: [],
-    optional: [],
-  },
+exports.userDelete = {
+	name: "userDelete",
+	description: "I delete a user",
+	inputs: {
+		required: ["userName", "password"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: true,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.users.delete(connection.params.userName, connection.params.password, function(error){
+			connection.error = error;
+			next(connection, true);
+		});
+	}
+};
 
-  run: function(api, connection, next){
-    // your logic here
-    next(connection, true);
-  }
+exports.usersList = {
+	name: "usersList",
+	description: "I list all the users",
+	inputs: {
+		required: [],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: false,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.users.list(function(error, users){
+			connection.error = error;
+			connection.response.users = [];
+			for(var i in users){
+				connection.response.users.push(users[i].userName)
+			}
+			next(connection, true);
+		});
+	}
+};
+
+exports.authenticate = {
+	name: "authenticate",
+	description: "I authenticate a user",
+	inputs: {
+		required: ["userName", "password"],
+		optional: [],
+	},
+	blockedConnectionTypes: [],
+	matchExtensionMimeType: false,
+	toDocument: true,
+	authenticated: false,
+	outputExample: {},
+	version: 1.0,
+	run: function(api, connection, next){
+		api.users.authenticate(connection.params.userName, connection.params.password, function(error, match){
+			connection.error = error;
+			connection.response.authenticated = match;
+			next(connection, true);
+		});
+	}
 };
